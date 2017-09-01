63b13cf4d092b62eef049a9ae4e7e785da6c0f8e
axs1088 <axs1088@psu.edu>
2017-09-01 15:20:41 -0400

    Adding a HPatient Class

---
+++ src/main/java/edu/psu/sweng500/emrms/model/HPatient.java
@@ -0,0 +1,86 @@
+package edu.psu.sweng500.emrms.model;
+
+//Added by Atul Singh 09/01/2017
+public class HPatient {
+    private long ObjectID;
+    private String userId;
+    private String creationDateTime;
+    private String mPINumber;
+    private Boolean organDonor;
+    private Boolean deceasedIndicator;
+    private Boolean isPatientUndentified;
+    private String primarylang;
+    private Boolean medHistoryConsent;
+
+    public long getObjectID() {
+        return ObjectID;
+    }
+
+    public void setObjectID(long objectID) {
+        ObjectID = objectID;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getCreationDateTime() {
+        return creationDateTime;
+    }
+
+    public void setCreationDateTime(String creationDateTime) {
+        this.creationDateTime = creationDateTime;
+    }
+
+    public String getmPINumber() {
+        return mPINumber;
+    }
+
+    public void setmPINumber(String mPINumber) {
+        this.mPINumber = mPINumber;
+    }
+
+    public Boolean getOrganDonor() {
+        return organDonor;
+    }
+
+    public void setOrganDonor(Boolean organDonor) {
+        this.organDonor = organDonor;
+    }
+
+    public Boolean getDeceasedIndicator() {
+        return deceasedIndicator;
+    }
+
+    public void setDeceasedIndicator(Boolean deceasedIndicator) {
+        this.deceasedIndicator = deceasedIndicator;
+    }
+
+    public Boolean getPatientUndentified() {
+        return isPatientUndentified;
+    }
+
+    public void setPatientUndentified(Boolean patientUndentified) {
+        isPatientUndentified = patientUndentified;
+    }
+
+    public String getPrimarylang() {
+        return primarylang;
+    }
+
+    public void setPrimarylang(String primarylang) {
+        this.primarylang = primarylang;
+    }
+
+    public Boolean getMedHistoryConsent() {
+        return medHistoryConsent;
+    }
+
+    public void setMedHistoryConsent(Boolean medHistoryConsent) {
+        this.medHistoryConsent = medHistoryConsent;
+    }
+}
