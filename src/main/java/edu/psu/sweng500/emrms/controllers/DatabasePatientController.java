e87228b6809fdf99f012eb57dfa36b6e824de3c0
Nilbog21 <aseefried@gmail.com>
2017-09-04 17:39:15 -0400

    Issue #1: Added first tests, for PatientController

---
+++ src/main/java/edu/psu/sweng500/emrms/controllers/DatabasePatientController.java
@@ -0,0 +1,20 @@
+package edu.psu.sweng500.emrms.controllers;
+
+import edu.psu.sweng500.emrms.models.HPatient;
+
+public class DatabasePatientController implements PatientController {
+    @Override
+    public void add(HPatient patient) {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    @Override
+    public void remove(String id) {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+
+    @Override
+    public HPatient getByUserId(String id) {
+        throw new UnsupportedOperationException("not yet implemented");
+    }
+}
---
+++ src/main/java/edu/psu/sweng500/emrms/controllers/PatientController.java
@@ -0,0 +1,10 @@
+package edu.psu.sweng500.emrms.controllers;
+
+import edu.psu.sweng500.emrms.exceptions.PatientNotFoundException;
+import edu.psu.sweng500.emrms.models.HPatient;
+
+public interface PatientController {
+    public void add(HPatient patient);
+    public void remove(String id) throws PatientNotFoundException;
+    public HPatient getByUserId(String id) throws PatientNotFoundException;
+}
