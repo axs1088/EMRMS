a68be181670dc5310ae639edbed63e7576bb6a5c
Nilbog21 <aseefried@gmail.com>
2017-09-10 17:42:49 -0400

    Changes during call

--- src/test/java/edu/psu/sweng500/emrms/services/PatientServiceTest.java
+++ src/test/java/edu/psu/sweng500/emrms/services/PatientServiceTest.java
@@ -4,6 +4,7 @@ import edu.psu.sweng500.emrms.service.PatientService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -11,15 +12,14 @@ import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {PatientService.class})
+@ContextConfiguration(locations = {"classpath:/spring/emrms-context.xml"})
 public class PatientServiceTest {
     @Autowired
-    // @Qualifier("patientService")
+    @Qualifier("patientService")
     private PatientService thisService;
 
     // @Test
     public void testSpringAnnotations() {
-
         //assert correct type/impl
         assertThat(thisService, instanceOf(PatientService.class));
     }
