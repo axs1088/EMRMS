0e1a5784101f0a730a3f4b4e9fe3a0cf7776fe68
axs1088 <axs1088@psu.edu>
2017-09-15 12:09:03 -0400

    Added Stored Procedure to search patient by demographics like FirstName/LastName and Gender.

--- Scripts/MySql/StoredProcedures/Emrms_FindPatientByDemographics.sql
+++ Scripts/MySql/StoredProcedures/Emrms_FindPatientByDemographics.sql
@@ -1,4 +1,4 @@
-Use EMRMS;
+      Use EMRMS;
 
 DELIMITER $$
   CREATE PROCEDURE Emrms_FindPatientByDemographics (IN lName VARCHAR(30),
@@ -7,13 +7,33 @@ DELIMITER $$
 
   BEGIN
 
-  IF lName <> '' AND fName <> '' AND gender <> ''
+  IF lName <> '' AND fName <> '' AND gender <> '' AND gender <> 3
   THEN
   Select hn.LastName, hn.FirstName, hp.Birthdate, hp.Gender, hpat.MPINo, henc.EncStartdateTime, henc.encStatus
   from h_name hn INNER JOIN h_person hp on hn.HpersonID= hp.HPersonID
   INNER JOIN H_Patient hpat on hpat.HPatientID = hp.HPersonID
   Left OUTER JOIN h_encounter henc on henc.Patient_ObjectID = hpat.HPatientID
   Where henc.encStatus = 1 and hn.LastName like CONCAT(lName,'%') and hn.FirstName like CONCAT(fName,'%') and hp.Gender = gender;
+  END IF;
+
+  /*Return All patients*/
+  IF lName <> '' AND fName <> '' AND gender = 3
+  THEN
+  Select hn.LastName, hn.FirstName, hp.Birthdate, hp.Gender, hpat.MPINo, henc.EncStartdateTime, henc.encStatus
+  from h_name hn INNER JOIN h_person hp on hn.HpersonID= hp.HPersonID
+  INNER JOIN H_Patient hpat on hpat.HPatientID = hp.HPersonID
+  Left OUTER JOIN h_encounter henc on henc.Patient_ObjectID = hpat.HPatientID
+  Where henc.encStatus = 1 and hn.LastName like CONCAT(lName,'%') and hn.FirstName like CONCAT(fName,'%');
+  END IF;
+
+  /*Return All patients*/
+  IF lName <> '' AND fName = '' AND gender = 3
+  THEN
+  Select hn.LastName, hn.FirstName, hp.Birthdate, hp.Gender, hpat.MPINo, henc.EncStartdateTime, henc.encStatus
+  from h_name hn INNER JOIN h_person hp on hn.HpersonID= hp.HPersonID
+  INNER JOIN H_Patient hpat on hpat.HPatientID = hp.HPersonID
+  Left OUTER JOIN h_encounter henc on henc.Patient_ObjectID = hpat.HPatientID
+  Where henc.encStatus = 1 and hn.LastName like CONCAT(lName,'%');
   END IF;
 
   END
