ad46ae3d97ce1fa6fc277caeb5ef736a7da85f74
axs1088 <axs1088@psu.edu>
2017-09-23 18:21:58 -0400

    Adding INSERT scripts for audit policies.

---
+++ Scripts/MySql/emrms-insert-audit-policies.sql
@@ -0,0 +1,24 @@
+INSERT INTO h_policy(PolicyName,PolicyID)
+  VALUES ('Login',1);
+INSERT INTO h_policy(PolicyName,PolicyID)
+  VALUES ('LogOff',2);
+INSERT INTO h_policy(PolicyName,PolicyID)
+  VALUES ('View Census',3);
+INSERT INTO h_policy(PolicyName,PolicyID)
+  VALUES ('Patient Locator',4);
+INSERT INTO h_policy(PolicyName,PolicyID)
+  VALUES ('Create a New Patient',5);
+INSERT INTO h_policy(PolicyName,PolicyID)
+  VALUES ('Create a New Encounter',6);
+INSERT INTO h_policy(PolicyName,PolicyID)
+  VALUES ('View Patient Details',7);
+INSERT INTO h_policy(PolicyName,PolicyID)
+  VALUES ('View Encounter Details',8);
+INSERT INTO h_policy(PolicyName,PolicyID)
+  VALUES ('View Patient Charts',9);
+INSERT INTO h_policy(PolicyName,PolicyID)
+  VALUES ('Add an Allergy',10);
+INSERT INTO h_policy(PolicyName,PolicyID)
+  VALUES ('Add an Assessment',11);
+INSERT INTO h_policy(PolicyName,PolicyID)
+  VALUES ('Add a Problem',12);
