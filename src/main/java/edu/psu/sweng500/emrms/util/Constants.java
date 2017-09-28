7332e3854bce173c9e6c6f91fc303bf23b2b8f7b
axs1088 <axs1088@psu.edu>
2017-09-28 14:16:16 -0400

    New constants added for Encounter Status, Bed Status and Staff Type.

--- src/main/java/edu/psu/sweng500/emrms/util/Constants.java
+++ src/main/java/edu/psu/sweng500/emrms/util/Constants.java
@@ -10,5 +10,19 @@ public final class Constants {
 	public static final String CENSUS_TYPE_NURSE = "NURSE CENSUS";
 	public static final String CENSUS_TYPE_PATIENT = "PATIENT CENSUS";
 	public static final String CENSUS = "census";
+
+	public static final int ENCOUNTER_STATUS_ACTIVE = 1;
+	public static final int ENCOUNTER_STATUS_CLOSED = 2;
+	public static final int ENCOUNTER_STATUS_SCHEDULED= 3;
+
+	public static final int STAFF_TYPE_PHYSICIAN = 1;
+	public static final int STAFF_TYPE_NURSE = 2;
+	public static final int STAFF_TYPE_OTHER = 3;
+
+	public static final int BED_STATUS_VACANT = 1;
+	public static final int BED_STATUS_OCCUPIED = 2;
+	public static final int BED_STATUS_HOUSEKEEPING = 3;
+
+
     
 }
