695f79ec514c4d3f70011fc9092789d0d89c90f9
axs1088 <axs1088@psu.edu>
2017-11-13 18:42:16 -0500

    Display one row per patient  in the census with the latest encounter information. Defect resolved. This was cauisng no response while clicking the patient name on the physician census.

--- Scripts/MySql/StoredProcedures/Emrms_GetNurseCensusList.sql
+++ Scripts/MySql/StoredProcedures/Emrms_GetNurseCensusList.sql
@@ -1,3 +1,4 @@
+
 USE EMRMS;
 
 CREATE PROCEDURE Emrms_GetNurseCensusList(IN LocationObjectID INT)
@@ -17,6 +18,8 @@ CREATE PROCEDURE Emrms_GetNurseCensusList(IN LocationObjectID INT)
     FROM h_name hn INNER JOIN h_person hp ON hn.HpersonID = hp.HPersonID
       INNER JOIN H_Patient hpat ON hpat.HPatientID = hp.HPersonID
       LEFT OUTER JOIN h_encounter henc ON henc.Patient_ObjectID = hpat.HPatientID
-    WHERE henc.encStatus = 1 AND henc.EncounterLocation_ObjectID = LocationObjectID;
+    WHERE henc.encStatus = 1 AND henc.EncounterLocation_ObjectID = LocationObjectID
+    AND henc.HEncounterID in(select MAX(HEncounterID) from h_encounter GROUP by Patient_ObjectID);
 
   END;
+
