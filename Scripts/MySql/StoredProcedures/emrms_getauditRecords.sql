364f4bdfc7fd006f4f5f1b829e63719bed8afb2f
axs1088 <axs1088@psu.edu>
2017-11-14 09:57:59 -0500

    Fixed the defect where StartDate and EndDate were not inclusive.

--- Scripts/MySql/StoredProcedures/emrms_getauditRecords.sql
+++ Scripts/MySql/StoredProcedures/emrms_getauditRecords.sql
@@ -4,13 +4,13 @@ CREATE PROCEDURE emrms_getauditRecords(IN SDateTime Varchar(30), IN EDTime Varch
   IF (SDateTime <> '' AND EDTime <> '') THEN
     SELECT AuditRecordID, UserId,  CreationDateTime, PolicyId, EventName, PatientName, Patient_ObjectID, Encounter_ObjectID, EncounterID
      FROM h_audit_record
-   Where CreationDateTime >= STR_TO_DATE(SDateTime, '%Y-%m-%d %H:%i:%s')
-      AND CreationDateTime <= STR_TO_DATE(EDTime, '%Y-%m-%d %H:%i:%s')
+   Where CreationDateTime >= STR_TO_DATE(SDateTime, '%Y-%m-%d')
+      AND CreationDateTime <= STR_TO_DATE(EDTime + interval 1 day, '%Y-%m-%d')
       AND CreationDateTime is not NULL AND PolicyId = PolId order by CreationDateTime DESC;
  ELSEIF (SDateTime <> '' AND EDTime = '') THEN
     SELECT AuditRecordID, UserId,  CreationDateTime, PolicyId, EventName, PatientName, Patient_ObjectID, Encounter_ObjectID, EncounterID
      FROM h_audit_record
-   Where CreationDateTime >= STR_TO_DATE(SDateTime, '%Y-%m-%d %H:%i:%s')
+   Where CreationDateTime >= STR_TO_DATE(SDateTime, '%Y-%m-%d')
       AND CreationDateTime is not NULL AND PolicyId = PolId order by CreationDateTime DESC;
   END IF;
 
