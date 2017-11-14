d419f0dee5d3c0c3ffec813704a4beb6cc519113
axs1088 <axs1088@psu.edu>
2017-11-14 09:17:09 -0500

    Fixed the logic to match HPatient.HPersonID to H_Person.HpersonID

--- Scripts/MySql/StoredProcedures/Emrms_GetNurseCensusList.sql
+++ Scripts/MySql/StoredProcedures/Emrms_GetNurseCensusList.sql
@@ -16,7 +16,7 @@ CREATE PROCEDURE Emrms_GetNurseCensusList(IN LocationObjectID INT)
       henc.encStatus,
       henc.HEncounterID
     FROM h_name hn INNER JOIN h_person hp ON hn.HpersonID = hp.HPersonID
-      INNER JOIN H_Patient hpat ON hpat.HPatientID = hp.HPersonID
+      INNER JOIN H_Patient hpat ON hpat.HPersonID = hp.HPersonID
       LEFT OUTER JOIN h_encounter henc ON henc.Patient_ObjectID = hpat.HPatientID
     WHERE henc.encStatus = 1 AND henc.EncounterLocation_ObjectID = LocationObjectID
     AND henc.HEncounterID in(select MAX(HEncounterID) from h_encounter GROUP by Patient_ObjectID);
